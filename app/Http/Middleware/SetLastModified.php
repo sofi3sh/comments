a8c6b7b399d15f594382ec3761042e6380f692b6
М <m@MacBook-Air-M.local>
2026-08-21 11:10:43 +0300

    fi crsf token on login

--- app/Http/Middleware/SetLastModified.php
+++ app/Http/Middleware/SetLastModified.php
@@ -24,6 +24,15 @@ class SetLastModified
         }
 
         $response->headers->set('Last-Modified', gmdate('D, d M Y H:i:s \\G\\M\\T', $timestamp));
+
+        if ($request->query->has('auth')) {
+            $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+            $response->headers->set('Pragma', 'no-cache');
+            $response->headers->set('Expires', '0');
+
+            return $response;
+        }
+
         $response->headers->set('Cache-Control', 'public, max-age=0, must-revalidate');
 
         if (! $request->isMethodCacheable()) {
